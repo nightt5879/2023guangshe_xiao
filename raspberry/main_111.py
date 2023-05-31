60273f33dfbace2de1f6df60898a3132489fadce
nightt5879 <1469259223@qq.com>
2023-05-31 22:33:05 +0800

    Update misc.xml

--- raspberry/main_111.py
+++ raspberry/main_111.py
@@ -1,14 +1,7 @@
 from main_program.util.mine_classify import MinesClassifier
 import cv2
 
-# mine_dict = {
-#     0: "蓝色三角",
-#     1: "蓝色圆形",
-#     2: "红色圆形",
-#     3: "红色三角",
-# }
-# # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
-# mine_classifier = MinesClassifier(paddle_model="./main_program/model/MobileNet_big.nb") #
+
 
 def classify_treasure(team = "red"):
     mine_dict = {
@@ -18,23 +11,37 @@ def classify_treasure(team = "red"):
         3: "红色三角",
     }
     mine_classifier = MinesClassifier(paddle_model="./main_program/model/MobileNet_big.nb")  #
-    cap = cv2.VideoCapture(1)
+    for i in range (5):
+        try:  # 多次重复调用有几率出错
+            cap = cv2.VideoCapture(1)
+            cap.set(4, 240)
+            cap.set(3, 320)
+            cam = "open"
+            break
+        except:
+            cam = "cannot open"
+            pass
+    if cam == "cannot open":  # 五次都打不开直接return假宝藏
+        print("Cannot open camera")
+        class_of = "fake"
+        return class_of
     list_of_treasure = [0,0,0,0,0] # 0:蓝色三角 1:蓝色圆形 2:红色圆形 3:红色三角 4:无类别
     while True:
         success, img = cap.read()
         if success:
             cv2.imshow("img", img)
             result, pro = mine_classifier.recognize_img(img)
-            if result == "蓝色三角" and pro[0] > 0.42:
+            # print(pro)
+            if mine_dict[result] == "蓝色三角" and pro[0] > 0.42:
                 print("蓝色三角")
                 list_of_treasure[0] += 1
-            elif result == "蓝色圆形" and pro[1] > 0.42:
+            elif mine_dict[result] == "蓝色圆形" and pro[1] > 0.42:
                 print("蓝色圆形")
                 list_of_treasure[1] += 1
-            elif result == "红色圆形" and pro[2] > 0.42:
+            elif mine_dict[result] == "红色圆形" and pro[2] > 0.42:
                 print("红色圆形")
                 list_of_treasure[2] += 1
-            elif result == "红色三角" and pro[3] > 0.42:
+            elif mine_dict[result] == "红色三角" and pro[3] > 0.42:
                 print("红色三角")
                 list_of_treasure[3] += 1
             else:
@@ -43,24 +50,32 @@ def classify_treasure(team = "red"):
             if list_of_treasure[0] > 10 or list_of_treasure[1] > 10 or \
                list_of_treasure[2] > 10 or list_of_treasure[3] > 10 or list_of_treasure[4] >10:
                 break
-            print(mine_dict[result], pro)
             cv2.waitKey(1)
-        # 分辨宝藏是真的还是假的,0:蓝色三角 1:蓝色圆形 2:红色圆形 3:红色三角
-        if team == "red" and list_of_treasure[2] > 10:
-            class_of = "fake"
-        elif team == "red" and list_of_treasure[3] > 10:
-            class_of = "true"
-        elif team == "blue" and list_of_treasure [0] > 10:
-            class_of = "fake"
-        elif team == "blue" and list_of_treasure [1] > 10:
-            class_of = "true"
-        else:
-            print("no treasure")
-            class_of = "fake"
-        return class_of
+    print(list_of_treasure)
+    # 分辨宝藏是真的还是假的,0:蓝色三角 1:蓝色圆形 2:红色圆形 3:红色三角
+    if team == "red" and list_of_treasure[2] > 10:
+        class_of = "fake"
+    elif team == "red" and list_of_treasure[3] > 10:
+        class_of = "true"
+    elif team == "blue" and list_of_treasure [0] > 10:
+        class_of = "fake"
+    elif team == "blue" and list_of_treasure [1] > 10:
+        class_of = "true"
+    else:
+        print("no treasure")
+        class_of = "fake"
+    return class_of
 
-a = classify_treasure()
+a = classify_treasure("blue")
 print(a)
+# mine_dict = {
+#     0: "蓝色三角",
+#     1: "蓝色圆形",
+#     2: "红色圆形",
+#     3: "红色三角",
+# }
+# # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
+# mine_classifier = MinesClassifier(paddle_model="./main_program/model/MobileNet_big.nb") #
 # cap = cv2.VideoCapture(1)
 # while True:
 #     success, img = cap.read()
--- raspberry/test_success_400_5_31.py
+++ raspberry/test_success_400_5_31.py
@@ -167,6 +167,69 @@ def left_90_with_stop(turn_time = 0.43):
     time.sleep(0.5)
     c.car_stop()
 
+def classify_treasure(team = "red"):
+    mine_dict = {
+        0: "蓝色三角",
+        1: "蓝色圆形",
+        2: "红色圆形",
+        3: "红色三角",
+    }
+    mine_classifier = MinesClassifier(paddle_model="./main_program/model/MobileNet_big.nb")  #
+    for i in range (5):
+        try:  # 多次重复调用有几率出错
+            cap = cv2.VideoCapture(1)
+            cap.set(4, 240)
+            cap.set(3, 320)
+            cam = "open"
+            break
+        except:
+            cam = "cannot open"
+            pass
+    if cam == "cannot open":  # 五次都打不开直接return假宝藏
+        print("Cannot open camera")
+        class_of = "fake"
+        return class_of
+    list_of_treasure = [0,0,0,0,0] # 0:蓝色三角 1:蓝色圆形 2:红色圆形 3:红色三角 4:无类别
+    while True:
+        success, img = cap.read()
+        if success:
+            cv2.imshow("img", img)
+            result, pro = mine_classifier.recognize_img(img)
+            # print(pro)
+            if mine_dict[result] == "蓝色三角" and pro[0] > 0.42:
+                print("蓝色三角")
+                list_of_treasure[0] += 1
+            elif mine_dict[result] == "蓝色圆形" and pro[1] > 0.42:
+                print("蓝色圆形")
+                list_of_treasure[1] += 1
+            elif mine_dict[result] == "红色圆形" and pro[2] > 0.42:
+                print("红色圆形")
+                list_of_treasure[2] += 1
+            elif mine_dict[result] == "红色三角" and pro[3] > 0.42:
+                print("红色三角")
+                list_of_treasure[3] += 1
+            else:
+                list_of_treasure[4] += 1
+            # sum of the list_of_treasure
+            if list_of_treasure[0] > 10 or list_of_treasure[1] > 10 or \
+               list_of_treasure[2] > 10 or list_of_treasure[3] > 10 or list_of_treasure[4] >10:
+                break
+            cv2.waitKey(1)
+    print(list_of_treasure)
+    # 分辨宝藏是真的还是假的,0:蓝色三角 1:蓝色圆形 2:红色圆形 3:红色三角
+    if team == "red" and list_of_treasure[2] > 10:
+        class_of = "fake"
+    elif team == "red" and list_of_treasure[3] > 10:
+        class_of = "true"
+    elif team == "blue" and list_of_treasure [0] > 10:
+        class_of = "fake"
+    elif team == "blue" and list_of_treasure [1] > 10:
+        class_of = "true"
+    else:
+        print("no treasure")
+        class_of = "fake"
+    return class_of
+
 if __name__ == '__main__':
     move_list = slove_path(path)
     #below is the pid value
@@ -233,12 +296,14 @@ if __name__ == '__main__':
             PIDLineTracking(K, Kp, Ki, Kd, Line, SumMax + 5000,SumMin,400,break_mod=1,break_time=break_time_long,user_max_time=5)
             # print("long line done")
             # detect the treasure
-            if treasure = "fake":  # 掉头就跑
+            treasure = classify_treasure()
+            print(treasure)
+            if treasure == "fake":  # 掉头就跑
                 left_90_with_stop()
                 left_90_with_stop(0.43)
-            elif treasure = "true": # hit the treasure
+            elif treasure == "true": # hit the treasure
                 PIDLineTracking(K=0.5, Kp=5, Ki=0, Kd=3, Line=120, SumMax=450, SumMin=100, base_speed=350,
-                                break_mod=1, break_time=break_time_short)
+                                break_mod=1, break_time=break_time_short + 30)
                 c.car_stop()
                 time.sleep(0.5)
                 hit_the_treasure(0.7)
@@ -248,7 +313,9 @@ if __name__ == '__main__':
             c.car_stop()
             time.sleep(0.3)
             # 观察宝藏
-            if treasure = "fake":
+            treasure = classify_treasure()
+            print(treasure)
+            if treasure == "fake":
                 if move_list[i+1][0] == "前进":  # 需要掉头
                     left_90_with_stop()
                     left_90_with_stop(0.4)
@@ -257,7 +324,7 @@ if __name__ == '__main__':
                     move_list[i+1][0] = "右转"
                 elif move_list[i+1][0] == "右转":
                     move_list[i+1][0] = "左转"
-            elif treasure = "real":
+            elif treasure == "true":
                 PIDLineTracking(K = 0.5, Kp = 5, Ki = 0, Kd = 3, Line = 120, SumMax = 450,SumMin = 100 ,base_speed = 350,
                                 break_mod=1,break_time=break_time_short)
                 # detect the treasure
