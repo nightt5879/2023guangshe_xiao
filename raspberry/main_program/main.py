9d379918df1af28bab631b73bbff129e0d0196e2
蔡依炼 <87569603+caiyilian@users.noreply.github.com>
2023-06-01 17:32:51 +0800

    Update main.py

--- raspberry/main_program/main.py
+++ raspberry/main_program/main.py
@@ -6,8 +6,8 @@ from util.mine_classify import MinesClassifier
 # 摄像头捕获视频识别出宝藏位置
 mine_points = get_loc()
 # 根据宝藏位置得到最终的总运动指令,optimize=True的话。最终路径就是真正最短的，但是用时可能更长
-path = get_paths(mine_points, optimize=False)
-print(path)
+planer = pathPlaner(mine_points)
+print(planer.paths)
 # 初始化激光雷达，用法是scan_data = my_lidar.get_data()。其中scan_data的值是一个列表，列表里面有360个元素分别对应0到359度范围的障碍物的距离
 my_lidar = Lidar()
 # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
