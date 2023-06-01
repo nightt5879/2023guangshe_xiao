900e06e7a976887e4f69be4c93112b7cae94c457
nightt5879 <1469259223@qq.com>
2023-06-01 20:51:57 +0800

    push

--- raspberry/main_program/main.py
+++ raspberry/main_program/main.py
@@ -1,9 +1,8 @@
 from util.get_map import get_loc
 from util.get_map import show_lcd
 from util.get_path import pathPlaner
-from util.lidar import Lidar
 from util.mine_classify import MinesClassifier
-
+# below is the other import
 import cv2
 import time
 import RPi.GPIO as GPIO
@@ -74,17 +73,58 @@ def select_team() -> str:
     return team_of
 
 
+def slove_path(path, hit_flag="hit") -> list:
+    """
+    根据路径规划的结果，进行路径的解决,每次只解决一条路径（使用pop取出 ）
+    :param path: 输入的路径
+    :param hit_flag: 是否撞击了宝藏
+    :return: 单条路径的list
+    """
+    move_list = []
+    one_path = []
+    # 9,1 11,3 1,5 7,5 19,7 9,9 11,9 15,9 5,11 9,11 11,11 1,13 13,15 19,15 9,17 11,19
+    non_intersection_points = [(9, 1), (11, 3), (1, 5), (7, 5), (19, 7), (9, 9), (11, 9), (15, 9), (5, 11), (9, 11),
+                               (11, 11), (1, 13), (13, 15), (19, 15), (9, 17), (11, 19)]
+    # 11,1 1,3 13,5 15,7 3,9 17,11 5,13 7,15 19,17 9,19
+    fork_road = [(11, 1), (1, 3), (13, 5), (15, 7), (3, 9), (17, 11), (5, 13), (7, 15), (19, 17), (9, 19)]
+    # 1，1 19，5 15，7 1，15 5，13 19，19
+    long_length_path = [(1, 1), (19, 5), (15, 7), (1, 15), (5, 13), (19, 19), (7, 7),
+                        (13, 13)]  # it means those path need one more move step the other are the short length path
+    for j in range(len(path)):  # path points
+        if path[j]["now_xy"] not in non_intersection_points:
+            one_path.append(path[j]["move_mode"])
+    if path[j]["now_xy"] in long_length_path and path[j]["move_mode"] == "前进":
+        one_path.pop()
+        one_path.append("长线")
+    else:
+        one_path.append("短线")
+    move_list.append(one_path)
+    one_path = []
+    print(move_list)
+    # if hit the treasure, the car turnover.so it need to change the direction
+    if hit_flag == "hit":
+        # print(i+1)
+        # print(move_list[i+1][0])
+        if move_list[0][0] == "左转" and move_list[0][1] == "左转":
+            # no need the turning
+            move_list[0].pop(0)
+            move_list[0].pop(0)
+        # change the direction
+        elif move_list[0][0] == "左转":
+            move_list[0][0] = "右转"
+        elif move_list[0][0] == "右转":
+            move_list[0][0] = "左转"
+    elif hit_flag == "no_hit":
+        pass
+    return move_list
+
+
 if __name__ == '__main__':
+    # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
+    mine_classifier = MinesClassifier(paddle_model="./model/MobileNet_big.nb")
     team = select_team()  # 本次比赛的队伍颜色
     mine_points = get_loc()  # 摄像头捕获视频识别出宝藏位置
     planer = pathPlaner(mine_points)  # 根据宝藏位置得到最终的总运动指令,optimize=True的话。最终路径就是真正最短的，但是用时可能更长
     print(mine_points)
     print(planer.paths)
-    # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
-    mine_classifier = MinesClassifier(paddle_model="./model/MobileNet_small.nb")
 
-    # # 初始化激光雷达，用法是scan_data = my_lidar.get_data()。其中scan_data的值是一个列表，列表里面有360个元素分别对应0到359度范围的障碍物的距离
-    # my_lidar = Lidar()
-    # while True:
-    #     # 获取激光雷达数据
-    #     print(my_lidar.get_data())
--- raspberry/main_program/test_in_main.py
+++ raspberry/main_program/test_in_main.py
@@ -75,16 +75,20 @@ def select_team() -> str:
 
 
 if __name__ == '__main__':
+    # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
+    mine_classifier = MinesClassifier(paddle_model="./model/MobileNet_small.nb")
     team = select_team()  # 本次比赛的队伍颜色
     mine_points = get_loc()  # 摄像头捕获视频识别出宝藏位置
     planer = pathPlaner(mine_points)  # 根据宝藏位置得到最终的总运动指令,optimize=True的话。最终路径就是真正最短的，但是用时可能更长
     print(mine_points)
     print(planer.paths)
-    # 初始化宝藏分类器，用法是：result = mine_classifier.recognize_img(img)。其中result的结果是0、1、2、3分别对应宝藏的四个类别。
-    mine_classifier = MinesClassifier(paddle_model="./model/MobileNet_small.nb")
 
-    # # 初始化激光雷达，用法是scan_data = my_lidar.get_data()。其中scan_data的值是一个列表，列表里面有360个元素分别对应0到359度范围的障碍物的距离
-    # my_lidar = Lidar()
-    # while True:
-    #     # 获取激光雷达数据
-    #     print(my_lidar.get_data())
+
+
+# from util.lidar import Lidar
+# # 初始化激光雷达，用法是scan_data = my_lidar.get_data()。其中scan_data的值是一个列表，列表里面有360个元素分别对应0到359度范围的障碍物的距离
+# my_lidar = Lidar()
+# while True:
+#     # 获取激光雷达数据
+#     print(my_lidar.get_data())
+
