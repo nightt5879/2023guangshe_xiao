cfa303ea64e8d5f1838d78c8561d8d8bbf0891e9
nightt5879 <1469259223@qq.com>
2023-06-03 22:33:22 +0800

    push

--- STM32/main_new/User/main.c
+++ STM32/main_new/User/main.c
@@ -45,41 +45,47 @@ int main(void)
 		// below are the serial port
 		if (Serial_GetRxFlag() == 1)  //get the data from serial port
 		{
-			OLED_ShowString(2, 1, "Count2:");
 			G1 = Serial_RxPacket[2] << 8 | Serial_RxPacket[3];
 			G2 = Serial_RxPacket[4] << 8 | Serial_RxPacket[5];
-			if (Serial_RxPacket[0] != 0x05)  // it mean not the base move
+//			OLED_ShowString(2, 1, "carmove:");
+			if(Serial_RxPacket[0] == 0x00 && Serial_RxPacket[1] == 0x00) //the car stop
 			{
-				if(Serial_RxPacket[0] == 0x00 && Serial_RxPacket[1] == 0x00) //the car stop
-				{
-					GPIO_Set(3);
-					GPIO_Set(6);
-				}
-				else if (Serial_RxPacket[0] == 0x01 && Serial_RxPacket[1] == 0x00) //the car forward
-				{
-					GPIO_Set(1);
-					GPIO_Set(4);
-				}
-				else if (Serial_RxPacket[0] == 0x02 && Serial_RxPacket[1] == 0x00) //the car back
-				{
-					GPIO_Set(2);
-					GPIO_Set(5);
-				}
-				else if (Serial_RxPacket[0] == 0x03 && Serial_RxPacket[1] == 0x00) //the car turn left
-				{
-					GPIO_Set(1);
-					GPIO_Set(5);
-				}
-				else if (Serial_RxPacket[0] == 0x04 && Serial_RxPacket[1] == 0x00) //the car turn right
-				{
-					GPIO_Set(2);
-					GPIO_Set(4);
-				}
+				GPIO_Set(3);
+				GPIO_Set(6);
+				PWM_SetCompara(1,G1);
+				PWM_SetCompara(2,G2);
+			}
+			else if (Serial_RxPacket[0] == 0x01 && Serial_RxPacket[1] == 0x00) //the car forward
+			{
+				GPIO_Set(1);
+				GPIO_Set(4);
+				PWM_SetCompara(1,G1);
+				PWM_SetCompara(2,G2);
+			}
+			else if (Serial_RxPacket[0] == 0x02 && Serial_RxPacket[1] == 0x00) //the car back
+			{
+				GPIO_Set(2);
+				GPIO_Set(5);
+				PWM_SetCompara(1,G1);
+				PWM_SetCompara(2,G2);
+			}
+			else if (Serial_RxPacket[0] == 0x03 && Serial_RxPacket[1] == 0x00) //the car turn left
+			{
+				GPIO_Set(1);
+				GPIO_Set(5);
+				PWM_SetCompara(1,G1);
+			    PWM_SetCompara(2,G2);
+			}
+			else if (Serial_RxPacket[0] == 0x04 && Serial_RxPacket[1] == 0x00) //the car turn right
+			{
+				GPIO_Set(2);
+				GPIO_Set(4);
 				PWM_SetCompara(1,G1);
 				PWM_SetCompara(2,G2);
 			}
 			else if (Serial_RxPacket[0] == 0x05)
 			{
+				OLED_ShowString(3, 1, "carmove222");
 				for	(int i = 1;i < 6;i++)  // get the 6050 data for 5 times
 				{
 					MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);
@@ -99,15 +105,15 @@ int main(void)
 				PWM_SetCompara(2,500);
 				while(1)  // get the 6050 data
 				{
-//					MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);
-//					del = GZ - start_GZ;
-//					OLED_ShowSignedNum(2, 1,start_GZ, 5);
-//					OLED_ShowSignedNum(3, 1, GZ, 5);
-//					OLED_ShowSignedNum(4, 1, acc_GZ, 5);
-//					if (fabs(del) > 200)  //Filter out noise.
-//					{
-//						acc_GZ += del;
-//					}
+					MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);
+					del = GZ - start_GZ;
+					OLED_ShowSignedNum(2, 1,start_GZ, 5);
+					OLED_ShowSignedNum(3, 1, GZ, 5);
+					OLED_ShowSignedNum(4, 1, acc_GZ, 5);
+					if (fabs(del) > 400)  //Filter out noise.
+					{
+						acc_GZ += del;
+					}
 					if (fabs(acc_GZ) > G1)
 					{
 						// stop the car
@@ -116,12 +122,12 @@ int main(void)
 						acc_GZ = 0; // reset the acc_GZ
 						break;
 					}
-					acc_GZ ++;
-					Delay_ms(10000);
-					GPIO_Set(3);
-					GPIO_Set(6);
-					acc_GZ = 0; // reset the acc_GZ
-					break;
+//					acc_GZ ++;
+//					Delay_ms(1000);
+//					GPIO_Set(3);
+//					GPIO_Set(6);
+//					acc_GZ = 0; // reset the acc_GZ
+//					break;
 				}
 			}
 		}
--- raspberry/test_anay.py
+++ raspberry/test_anay.py
@@ -4,7 +4,12 @@ import time
 c = move.Car()
 c.car_stop()
 print("init done")
-c.car_turn_left_6050(100)
+# c.car_forward(400,400)
+# time.sleep(2)
+c.car_turn_right_6050(39000)
+for i in range(12):
+    c.car_turn_right_6050(39000)
+    time.sleep(2)
 # c.car_turn_left(400,400)
 # time.sleep(10)
 # c.car_stop()
