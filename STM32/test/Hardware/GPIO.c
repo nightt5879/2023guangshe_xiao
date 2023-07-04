59ed504f23c34a6b8ed09fb5f0237bc3093d494b
Nightt <87569709+nightt5879@users.noreply.github.com>
2023-07-04 13:02:08 +0800

    Update GPIO.c

--- STM32/test/Hardware/GPIO.c
+++ STM32/test/Hardware/GPIO.c
@@ -7,11 +7,14 @@ void GPIOInit(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 |GPIO_Pin_5 |GPIO_Pin_6 |GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 |GPIO_Pin_5 |GPIO_Pin_6 |GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 |GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5  | GPIO_Pin_13 | GPIO_Pin_14 |GPIO_Pin_15;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;  //IN_FLOATING
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11| GPIO_Pin_12;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
 void GPIO_Set(uint8_t CHx)  //control the motor rotation
