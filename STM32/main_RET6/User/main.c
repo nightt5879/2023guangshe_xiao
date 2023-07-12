2e7adb0e99e738722a9542d960455917a099022d
Nightt <87569709+nightt5879@users.noreply.github.com>
2023-07-12 17:11:35 +0800

    Create Target_1_STM32F103C8_1.0.0.dbgconf

---
+++ STM32/main_RET6/User/main.c
@@ -0,0 +1,17 @@
+#include "stm32f10x.h"                  // Device header
+
+int main(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+//	GPIO_SetBits(GPIOC, GPIO_Pin_13);
+	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+	while (1)
+	{
+		
+	}
+}
