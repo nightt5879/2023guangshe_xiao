41bac6901a5a9fff09b6aaaf8696857643d7d9cc
Nightt <87569709+nightt5879@users.noreply.github.com>
2023-07-18 17:07:17 +0800

    Update test_in_main.py

--- raspberry/main_program/test_in_main.py
+++ raspberry/main_program/test_in_main.py
@@ -1,4 +1,6 @@
 # 包的导入
+import os
+os.chdir("/home/pi/Desktop/main_program")
 from util.get_map import get_loc
 from util.get_map import show_lcd
 from util.get_path2 import pathPlaner
@@ -13,21 +15,36 @@ import RPi.GPIO as GPIO
 import move
 from YYJ import GPIO_RPi
 from YYJ import Vision
-import os
 import sys
 from move import path
 from util.countdown import countdown
+from util import servo
+import threading
 # 全局参数
 BUTTON_INPUT = 21  # 按钮连接的GPIO口
+SERVO_PIN_TOP = 24  # 云台舵机1连接的GPIO口
+SERVO_PIN_MEDIUM = 23  # 云台舵机2连接的GPIO口
+SERVO_PIN_BOTTOM = 18  # 云台舵机3连接的GPIO口
+rotate_angle = 0  # 云台旋转的角度
+
 
 def GPIO_init():
     """
     初始化所有的树莓派GPIO引脚
     """
+    global rotate_angle, servo_top, servo_medium, servo_bottom
     # 按键输入的init
     GPIO.setmode(GPIO.BCM)
     BUTTON_PIN = BUTTON_INPUT
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    servo_top = servo.HalfCircleServo(SERVO_PIN_TOP)
+    servo_medium = servo.HalfCircleServo(SERVO_PIN_MEDIUM)
+    servo_bottom = servo.HalfCircleServo(SERVO_PIN_BOTTOM)
+    # set the servo's rotate angle
+    servo_top.target = 90
+    servo_medium.target = 0
+    servo_bottom.target = 0
+
 
 def button_with_wait():
     """
@@ -88,19 +105,50 @@ def select_team():
     return team_of
 
 
+def control_servo(vertical_servo, vertical_angle: int, horizontal_angle: int):
+    global rotate_angle
+    vertical_servo.target = vertical_angle
+    rotate_angle = horizontal_angle
+
+
+def thread_nodding():
+    while True:
+        if servo_top.target != servo_top.angle:
+            servo_top.set_angle(servo_top.target)
+        time.sleep(0.5)
+
+
+def thread_rotating():
+    global rotate_angle
+    while True:
+        if rotate_angle != servo_medium.angle + servo_bottom.angle:
+            rotate_angle = servo_medium.clamp_number(rotate_angle, 0, 360)
+            if 0 <= rotate_angle <= 180:
+                # check the servo_bottom's angle
+                if servo_bottom.angle != 0:
+                    servo_bottom.set_angle(0)
+                servo_medium.set_angle(rotate_angle)
+            elif 180 < rotate_angle <= 360:
+                servo_medium.set_angle(180)
+                # print(rotate_angle)
+                servo_bottom.set_angle(rotate_angle - 180)
+        time.sleep(0.5)
+            
 if __name__ == '__main__':
     GPIO_init()  # 初始化GPIO
+    # 启动舵机子线程
+    t1 = threading.Thread(target=thread_rotating)
+    t2 = threading.Thread(target=thread_nodding)
+    t1.start()
+    t2.start()
     team = select_team() # 选择队伍
-    mine_points,mine_img = get_loc()  # 摄像头捕获视频识别出宝藏位置
-     # 实例化一个地图架构解析对象
-    mapAnalysiser = MapArchRecognizer(mine_img)
-    # 转换地图得到21 * 21的矩阵
-    map_array = mapAnalysiser.analysis_map()
-    # 把识别出来的21 * 21矩阵保存起来（这一步必须在planner对象初始化之前完成）
-    cv2.imwrite("./imgs/small_labyrinth.png", map_array)
+    control_servo(servo_top, 90, 90)
+    mine_points,mine_img = get_loc()    # 摄像头捕获视频识别出宝藏位置
+    mapAnalysiser = MapArchRecognizer(mine_img)      # 实例化一个地图架构解析对象
+    map_array = mapAnalysiser.analysis_map()        # 转换地图得到21 * 21的矩阵
+    cv2.imwrite("./imgs/small_labyrinth.png", map_array)     # 把识别出来的21 * 21矩阵保存起来（这一步必须在planner对象初始化之前完成）
     time.sleep(1)
     planer = pathPlaner(mine_points,team)  # 根据宝藏位置得到最终的总运动指令,optimize=True的话。最终路径就是真正最短的，但是用时可能更长
     print(planer.paths_list)
-    countdown(10)  # 倒计时
+    # countdown(10)  # 倒计时
     # print(1)
-    # planer = pathPlaner(mine_points)  # 根据宝藏位置得到最终的总运动指令,optimize=True的话。最终路径就是真正最短的，但是用时可能更长
